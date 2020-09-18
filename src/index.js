9860c178587c64b7d2f4321c11b1d747122f96ba
roshanronan <singhroshandu@gmail.com>
2020-09-19 01:51:35 +0530

    server api

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { BrowserRouter } from "react-router-dom";
 import { ApolloProvider } from "@apollo/react-hooks";
+import Config from "./Config/config";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000",
+  uri: Config.API_URL,
 });
 const cache = new InMemoryCache();
 
@@ -42,10 +43,9 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-export {client}
+export { client };
